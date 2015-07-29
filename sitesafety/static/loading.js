606c92f337345ef7c465def9c363d0b1ffd9cb6e
lydianchord <lydianchord@users.noreply.github.com>
2015-07-29 12:30:49 -0500

    Added Flask badge, modified loading message

--- sitesafety/static/loading.js
+++ sitesafety/static/loading.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
   document.getElementById("submit").addEventListener("click", function () {
-    var p = "<p>Loading...</p>";
+    var p = "<hr><p>Loading...</p><hr>";
     var results = document.getElementById("results");
     document.getElementsByTagName("form")[0].insertAdjacentHTML("afterend", p);
     if (results) {
