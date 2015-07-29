c5164076a550d0094bf0c3c3e43e27ad9ed9b522
lydianchord <lydianchord@users.noreply.github.com>
2015-07-28 23:55:23 -0500

    Wrote helper method for tests

--- sitesafety/tests.py
+++ sitesafety/tests.py
@@ -5,35 +5,31 @@ import sitesafety_app
 
 class SiteTestCase(unittest.TestCase):
     
+    def assert_status_code_200(self, url):
+        with self.app.head(url) as resp:
+            self.assertEqual(resp.status_code, 200)
+    
     def setUp(self):
         sitesafety_app.app.config['TESTING'] = True
         self.app = sitesafety_app.app.test_client()
     
     def test_request_index(self):
-        with self.app.head('/') as resp:
-            self.assertEqual(resp.status_code, 200)
+        self.assert_status_code_200('/')
     
     def test_valid_search(self):
-        with self.app.head('/check?site=nicovideo.jp') as resp:
-            self.assertEqual(resp.status_code, 200)
-        with self.app.head('/check?site=http://www.nicovideo.jp/') as resp:
-            self.assertEqual(resp.status_code, 200)
+        self.assert_status_code_200('/check?site=nicovideo.jp')
+        self.assert_status_code_200('/check?site=http://www.nicovideo.jp/')
     
     def test_valid_but_strange_search(self):
-        with self.app.head('/check?site=nicovideo.j') as resp:
-            self.assertEqual(resp.status_code, 200)
+        self.assert_status_code_200('/check?site=nicovideo.j')
     
     def test_invalid_search(self):
-        with self.app.head('/check?site=nico video.') as resp:
-            self.assertEqual(resp.status_code, 200)
-        with self.app.head('/check?site=nicovideo') as resp:
-            self.assertEqual(resp.status_code, 200)
+        self.assert_status_code_200('/check?site=nico video.')
+        self.assert_status_code_200('/check?site=nicovideo')
     
     def test_no_arguments(self):
-        with self.app.head('/check') as resp:
-            self.assertEqual(resp.status_code, 200)
-        with self.app.head('/check?site=') as resp:
-            self.assertEqual(resp.status_code, 200)
+        self.assert_status_code_200('/check')
+        self.assert_status_code_200('/check?site=')
 
 
 if __name__ == '__main__':
